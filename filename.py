cdee10375d6f0e7e8a55969d6ab3492671b0018f
Jeevitha1407 <85873519+Jeevitha1407@users.noreply.github.com>
2021-06-14 16:16:59 +0530

    Create filename.py

---
+++ filename.py
@@ -0,0 +1,5 @@
+1  filename=input("input the filename:")
+2  f_extns=filename.split(".")
+3  print("the extension of the file is :
+4  input the filename:abc.py
+5  the extension of the file is :'py'
